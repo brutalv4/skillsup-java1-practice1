625bc4d42fa05898608cef10c0452242759b7dbf
Vladimir Bredikhin <brutal.v4@gmail.com>
2017-08-13 20:13:46 +0300

    ApplicationContext switched from ClassPathXml to AnnotationConfig

--- AuctionCore/src/main/java/skillsup/practice/spring/core/App.java
+++ AuctionCore/src/main/java/skillsup/practice/spring/core/App.java
@@ -1,7 +1,9 @@
 package skillsup.practice.spring.core;
 
+import com.google.gson.Gson;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import skillsup.practice.spring.core.config.SpringConfig;
 import skillsup.practice.spring.dao.ItemDao;
 import skillsup.practice.spring.dao.UserDao;
 import skillsup.practice.spring.shared.model.Item;
@@ -9,19 +11,21 @@ import skillsup.practice.spring.shared.model.User;
 
 public class App {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("/core/core-context.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
-		AuctionService auctionService = context.getBean(AuctionService.class);
-		ItemDao itemDao = context.getBean(ItemDao.class);
-		Item item = itemDao.findAll().get(0);
+        AuctionService auctionService = context.getBean(AuctionService.class);
+        ItemDao itemDao = context.getBean(ItemDao.class);
+        Item item = itemDao.findAll().get(0);
 
-		UserDao userDao = context.getBean(UserDao.class);
-		User user = userDao.findAll().get(0);
+        UserDao userDao = context.getBean(UserDao.class);
+        User user = userDao.findAll().get(0);
 
-		auctionService.createLot(item, user);
-		auctionService.getAllActiveLots().forEach(System.out::println);
+        auctionService.createLot(item, user);
+        auctionService.getAllActiveLots().forEach(System.out::println);
 
-	}
+        Gson gson = (Gson) context.getBean("gson");
+        System.out.println(gson.toJson(auctionService));
+    }
 }
